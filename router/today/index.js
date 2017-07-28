cffe3da39f8d6bf2ac717c45776558e759140e91
eazyjobb <kararyou@qq.com>
2017-07-28 15:44:38 +0800

    today page

--- router/today/index.js
+++ router/today/index.js
@@ -10,7 +10,86 @@ router.get('/', function (req, res) {
 			title: '今日事',
 			description: '<p>又急又气，正在施工中</p><p>没有logo，你奈我何</p>'
 		}),
-		content: render.today(),
+		content: render.today({
+			happiness: [{
+				author: "Ng",
+				description: "末日尼哥上线了",
+				date: new Date('2017/07/28')
+			},{
+				author: "Ng",
+				description: "末日尼哥上线了",
+				date: new Date('2017/07/28')
+			},{
+				author: "Ng",
+				description: "末日尼哥上线了",
+				date: new Date('2017/07/28')
+			},{
+				author: "Ng",
+				description: "末日尼哥上线了",
+				date: new Date('2017/07/28')
+			},{
+				author: "Ng",
+				description: "末日尼哥上线了",
+				date: new Date('2017/07/28')
+			},{
+				author: "Ng",
+				description: "末日尼哥上线了末日尼哥上线了末日尼哥上线了末日尼哥上线了末日尼哥上线了末日尼哥上线了末日尼哥上线了末日尼哥上线了末日尼哥上线了",
+				date: new Date('2017/07/28')
+			},{
+				author: "Ng",
+				description: "末日尼哥上线了",
+				date: new Date('2017/07/28')
+			},{
+				author: "Ng",
+				description: "末日尼哥上线了",
+				date: new Date('2017/07/28')
+			},{
+				author: "Ng",
+				description: "末日尼哥上线了",
+				date: new Date('2017/07/28')
+			},{
+				author: "Ng",
+				description: "末日尼哥上线了",
+				date: new Date('2017/07/28')
+			}],
+			sadness: [{
+				author: "Ng",
+				description: "好想吃螺蛳粉",
+				date: new Date('2017/07/28')
+			},{
+				author: "Ng",
+				description: "好想吃螺蛳粉好想吃螺蛳粉",
+				date: new Date('2017/07/28')
+			},{
+				author: "Ng",
+				description: "好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉",
+				date: new Date('2017/07/28')
+			},{
+				author: "Ng",
+				description: "好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉",
+				date: new Date('2017/07/28')
+			},{
+				author: "Ng",
+				description: "好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉",
+				date: new Date('2017/07/28')
+			},{
+				author: "Ng",
+				description: "好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉",
+				date: new Date('2017/07/28')
+			},{
+				author: "Ng",
+				description: "好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉",
+				date: new Date('2017/07/28')
+			},{
+				author: "Ng",
+				description: "好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉",
+				date: new Date('2017/07/28')
+			},{
+				author: "Ng",
+				description: "好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉好想吃螺蛳粉",
+				date: new Date('2017/07/28')
+			}],
+		}),
 		bottom: render.bottom()
 	}));
 });
--- static/js/today.js
+++ static/js/today.js
@@ -2,4 +2,77 @@ $(document).ready(function () {
 	$(document).on('touchstart touchend', '.reveal', function() {
     	$('.reveal').click();
 	});
+
+	$('.visible').click(function() {console.log('bug');});
+	
+	$('.reveal').ready(function () {
+
+		var mid_offset = $('.ng-divider').width() / 1.5 * 1.75;
+
+		var left_before = $('<div class="div-line">').css({
+			'top'		: '50px',
+			'right'		: mid_offset + 'px'
+		});
+
+		var left_after = $('<div class="div-line">').css({
+			'bottom'	: '50px',
+			'right'		: mid_offset + 'px'
+		});
+
+		var right_before = $('<div class="div-line">').css({
+			'top'		: '50px',
+			'left'		: mid_offset + 'px'
+		});
+
+		var right_after = $('<div class="div-line">').css({
+			'bottom'	: '50px',
+			'left'		: mid_offset + 'px'
+		});
+
+
+		$('#left-hidden .ng-divider').before(left_before);
+		$('#left-hidden .ng-divider').after(left_after);
+		$('#right-hidden .ng-divider').before(right_before);
+		$('#right-hidden .ng-divider').after(right_after);
+
+		setTimeout(
+			"$('.dimmer').removeClass('active').addClass('disabled')",
+			500
+		);
+
+		putWords = function () {
+			var beginTop = 50;
+			var count = $('.words.left').length;
+			var overFlowHeight = $('.today-type2').height() - 100;
+			for (var i = 0; i < count; ++ i) {
+				var blockHeight = $('.words.left:eq('+i+')').height() + 30;
+
+				if (beginTop + blockHeight > overFlowHeight)
+					$('.words.left:eq('+i+')').hide();
+				else
+					$('.words.left:eq('+i+')').show();
+
+				$('.words.left:eq('+i+')').css({'top' : beginTop + 'px'});
+				beginTop += blockHeight + 25;
+			}
+
+			beginTop = 50;
+			count = $('.words.right').length;
+			for (var i = 0; i < count; ++ i) {
+				var blockHeight = $('.words.right:eq('+i+')').height() + 30;
+
+				if (beginTop + blockHeight > overFlowHeight)
+					$('.words.right:eq('+i+')').hide();
+				else
+					$('.words.right:eq('+i+')').show();
+
+				$('.words.right:eq('+i+')').css({'top' : beginTop + 'px'});
+				beginTop += blockHeight + 25;
+			}
+		};
+
+		putWords();
+		$(window).resize(putWords)
+	});
+	
 });
