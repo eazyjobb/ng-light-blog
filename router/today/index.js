a97d99b6b1579814fcf846f8c84f5bd0167c1541
eazyjobb <kararyou@qq.com>
2017-07-31 14:47:45 +0800

    i hate myself

--- router/today/index.js
+++ router/today/index.js
@@ -78,7 +78,7 @@ router.get('/history/data/', function (req, res) {
 							.then(function (result) {
 								happiness.push({
 									author:result.name,
-									description: entry.description
+									description: entry.description,
 									type: entry.type
 								});
 							})
